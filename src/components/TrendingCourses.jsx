31b55af3bf5957252f9e3a64f75ccc38e670c119
nathbiswa <biswanroy29@gmail.com>
2026-05-04 05:09:48 +0600

    trending sectiion data fetch and show done

--- src/components/TrendingCourses.jsx
+++ src/components/TrendingCourses.jsx
@@ -3,23 +3,24 @@
 import CardProducts from "@/components/CardProducts";
 
 const TrendingCourses = async () => {
-    const res = await fetch("https://coder-two-neon.vercel.app/all-course.json", {
+    const res = await fetch("https://coder-two-neon.vercel.app/new-courses.json", {
         cache: "no-store",
     });
 
     const courses = await res.json();
 
 
-    const trending = courses.slice(6, 12);
+    // const trending = courses.slice(0, 6);
 
     return (
         <div className="my-16">
-            <h2 className="text-3xl font-bold text-center mb-6">
-                Trending Courses
+            <h2 className="text-[14px] md:text-3xl font-bold text-center mb-6">
+                Trending Courses & UP Coming soon...
             </h2>
+            <p className="text-[10px] md:text-[15px] text-center mb-6">There is very Extaited Feature... <br /> Mind blowing suppor or Guideline.</p>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                {trending.map((course) => (
+                {courses.map((course) => (
                     <CardProducts key={course.id} course={course} />
                 ))}
             </div>
